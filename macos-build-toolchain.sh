28e05eecf21e6dfcbe3a31694016ae4bcc82e342
Tom Storey <7754310+tomstorey@users.noreply.github.com>
2021-09-26 14:36:48 +0100

    Update gcc/binutils versions, path changes
    Builds gcc into a directory by its version number

--- macos-build-toolchain.sh
+++ macos-build-toolchain.sh
@@ -6,53 +6,56 @@
 # Original source:
 #     https://gist.github.com/deplinenoise/bcfa4fb9964f90a5f3e9d69f08ca4464
 #
-# I have modified it only to use more recent versions of gcc and binutils, and
-# to include libmpc in the list of packages for brew to install.
+# I have modified it only to use more recent versions of gcc and binutils, to
+# include libmpc in the list of packages for brew to install, and create
+# directories within the toolchain directory instead of the working directory.
 ###############################################################################
 
 echo Building GCC m68k toolchain...
 
-mkdir -p sources
-mkdir -p build
+mkdir -p toolchain
+mkdir -p toolchain/sources
+mkdir -p toolchain/build
 
 CORES=8
 TARGET=m68k-eabi-elf
-PREFIX=$PWD/$TARGET
+PREFIX=$PWD/toolchain/$TARGET
 
 MIRROR=http://ftpmirror.gnu.org
 
-BINUTILS=binutils-2.36
+BINUTILS=binutils-2.37
 BINUTILS_URL=$MIRROR/binutils/$BINUTILS.tar.xz
 
-GCC=gcc-10.2.0
+GCCVER=11.2.0
+GCC=gcc-$GCCVER
 GCC_URL=$MIRROR/gcc/$GCC/$GCC.tar.xz
 
 brew install wget mpfr mpc libmpc gmp # Can't check this, because brew errors if things are already installed.
 
-if [ ! -f sources/$BINUTILS.tar.xz ]; then
+if [ ! -f toolchain/sources/$BINUTILS.tar.xz ]; then
   echo Fetching $BINUTILS_URL
-  (cd sources && wget $BINUTILS_URL) || exit 1
+  (cd toolchain/sources && wget $BINUTILS_URL) || exit 1
 fi
 
-if [ ! -f sources/$GCC.tar.xz ]; then
+if [ ! -f toolchain/sources/$GCC.tar.xz ]; then
   echo Fetching $GCC_URL
-  (cd sources && wget $GCC_URL) || exit 1
+  (cd toolchain/sources && wget $GCC_URL) || exit 1
 fi
 
-test -d sources/$BINUTILS || (cd sources && tar xjvf ../sources/$BINUTILS.tar.xz) || exit 1
-test -d sources/$GCC || (cd sources && tar xjvf ../sources/$GCC.tar.xz) || exit 1
-mkdir -p build
+test -d toolchain/sources/$BINUTILS || (cd toolchain/sources && tar xjvf ../sources/$BINUTILS.tar.xz) || exit 1
+test -d toolchain/sources/$GCC || (cd toolchain/sources && tar xjvf ../sources/$GCC.tar.xz) || exit 1
+mkdir -p toolchain/build
 
-if [ ! -f $PREFIX/bin/$TARGET-nm ]; then
+if [ ! -f $PREFIX-$GCCVER/bin/$TARGET-nm ]; then
   echo Building binutils
-  rm -rf build/binutils
-  mkdir -p build/binutils
-  (cd build/binutils && ../../sources/$BINUTILS/configure --target=$TARGET --disable-werror --prefix=$PREFIX && make -j $CORES && make install) || exit 1
+  rm -rf toolchain/build/binutils
+  mkdir -p toolchain/build/binutils
+  (cd toolchain/build/binutils && ../../sources/$BINUTILS/configure --target=$TARGET --disable-werror --prefix=$PREFIX-$GCCVER && make -j $CORES && make install) || exit 1
 fi
 
-if [ ! -f $PREFIX/bin/$TARGET-gcc ]; then
+if [ ! -f $PREFIX-$GCCVER/bin/$TARGET-gcc ]; then
   echo Building GCC
-  rm -rf build/gcc
-  mkdir -p build/gcc
-  (cd build/gcc && ../../sources/$GCC/configure --target=$TARGET --disable-werror --prefix=$PREFIX --enable-languages=c --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpc=/usr/local && make -j $CORES all-gcc && make install-gcc) || exit 1
+  rm -rf toolchain/build/gcc
+  mkdir -p toolchain/build/gcc
+  (cd toolchain/build/gcc && ../../sources/$GCC/configure --target=$TARGET --disable-werror --prefix=$PREFIX-$GCCVER --enable-languages=c,c++,go --with-gmp=/usr/local --with-mpfr=/usr/local --with-mpc=/usr/local && make -j $CORES all-gcc && make install-gcc) || exit 1
 fi
